d9d95d7f8e28371f5c3d9b6adbe94cc38496925f
Mike Alche <mikealche@gmail.com>
2021-02-27 22:55:04 -0300

    Improve index

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,13 +1,43 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { routes, authenticateAPI } from "@template/shared";
-import Cookies from "js-cookie";
-import { useAuth } from "../contexts/auth";
-import { useRouter } from "next/router";
+import Image from "next/image";
 import MainLayout from "../layouts/MainLayout";
-import { useEffect } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import Link from "next/link";
 export default function Home() {
-  const router = useRouter();
-
-  return <MainLayout>"hola"</MainLayout>;
+  return (
+    <MainLayout>
+      <Row className="justify-content-md-center">
+        <Col xs={6} style={{ minHeight: 200, marginTop: 100 }}>
+          <Image src="/nextadonis.jpg" layout="fill" objectFit="contain" />
+        </Col>
+      </Row>
+      <Row xs={10} className="justify-content-md-center">
+        <Col>
+          <h1>
+            Welcome to this Fullstack Next.js + Adonis.js starter template
+          </h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col xs={4}>
+          <Link href="/login">
+            <Button
+              block
+              className="mt-4 gradient"
+              size="lg"
+              variant="outline-primary"
+            >
+              Login
+            </Button>
+          </Link>
+        </Col>
+        <Col xs={4}>
+          <Link href="/signup">
+            <Button block type="submit" className="mt-4 gradient" size="lg">
+              Signup
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+    </MainLayout>
+  );
 }
